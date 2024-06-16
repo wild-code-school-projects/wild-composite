f1ca8c14a1e610eb3305eda914698cab105699ca
SuperCoolNinja <sbpro570@gmail.com>
2024-06-16 11:27:50 +0200

    init

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace AtelierComposite
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public List<Person> Children { get; set;}
+
+        public Person(string name)
+        {
+            Name = name;
+            Children = new List<Person>();
+        }
+
+        public void AddChildren(Person children)
+        {
+            Children.Add(children);
+        }
+
+        public override string ToString()
+        {
+            return ToString(0);
+        }
+
+        private string ToString(int level)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(new string(' ', level * 2) + Name);
+            foreach (var descendant in Children)
+            {
+                sb.Append(descendant.ToString(level + 1));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AtelierComposite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person root = new Person("John");
+            Person child1 = new Person("Alice");
+            Person child2 = new Person("Bob");
+            Person grandChild1 = new Person("Charlie");
+            Person grandChild2 = new Person("Diana");
+
+            root.AddChildren(child1);
+            root.AddChildren(child2);
+            child1.AddChildren(grandChild1);
+            child2.AddChildren(grandChild2);
+
+            Console.WriteLine(root.ToString());
+        }
+    }
+}
